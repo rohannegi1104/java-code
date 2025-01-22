60407f87515ad91266ea79d3c2caa634710b2007
rohannegi1104 <rohansingh6814@gmail.com>
2025-01-22 11:53:31 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("**************************************");
-		System.out.println("Hello from GITHub");
-		System.out.println("**************************************");
-	}
+    public static void main(String[] args) {
+        System.out.println("**************************************");
+        System.out.println("Hello Plasma!");
+        System.out.println("This Java project is integrated with Jenkins and Docker.");
+        System.out.println("**************************************");
+    }
 
 }
