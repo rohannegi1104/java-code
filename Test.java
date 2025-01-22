154f3dc36e35bbc89da02f263e7f969ffd03aff7
rohannegi1104 <rohansingh6814@gmail.com>
2025-01-23 00:51:33 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,15 @@
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("**************************************");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("**************");
         System.out.println("Hello Plasma!");
         System.out.println("This Java project is integrated with Jenkins and Docker.");
-        System.out.println("**************************************");
+        System.out.println("**************");
+
+        // Keep the program running
+        while (true) {
+            Thread.sleep(1000); // Sleep for 1 second
+        }
     }
 
 }
